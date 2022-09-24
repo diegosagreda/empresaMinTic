27b3e5518f0b3d2c4ecfd628ef9fc3ae5f468ddd
diegosagreda <diegoagreda1996@gmail.com>
2022-09-24 16:32:25 -0500

    Cambio

--- src/main/java/com/example/empresamintic/controllers/EmpleadoController.java
+++ src/main/java/com/example/empresamintic/controllers/EmpleadoController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 //@RequestMapping("/ver/empresas")
 public class EmpleadoController {
-
+    //dueg
     @Autowired
     private EmpleadoService empleadoService;
 
