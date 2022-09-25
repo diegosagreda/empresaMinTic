750ab620f49ce5d7f3cd9fdff0bf0e1f9e2dd1ff
diegosagreda <diegoagreda1996@gmail.com>
2022-09-24 20:29:15 -0500

    Nueva BD 3

--- src/main/java/com/example/empresamintic/EmpresaMinticApplication.java
+++ src/main/java/com/example/empresamintic/EmpresaMinticApplication.java
@@ -18,7 +18,7 @@ public class EmpresaMinticApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("/**").allowedMethods("*").allowedHeaders("*");
+                registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5173/").allowedMethods("*").allowedHeaders("*");
             }
         };
     }
