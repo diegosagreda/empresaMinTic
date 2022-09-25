f6ef58239fc524f4ec84c92aedc75c23015bf86d
diegosagreda <diegoagreda1996@gmail.com>
2022-09-24 20:44:53 -0500

    Nueva BD 4

--- src/main/java/com/example/empresamintic/EmpresaMinticApplication.java
+++ src/main/java/com/example/empresamintic/EmpresaMinticApplication.java
@@ -18,7 +18,7 @@ public class EmpresaMinticApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://127.0.0.1:5173/").allowedMethods("*").allowedHeaders("*");
+                registry.addMapping("/**").allowedOrigins("https://silly-biscochitos-2c9ef7.netlify.app/").allowedMethods("*").allowedHeaders("*");
             }
         };
     }
